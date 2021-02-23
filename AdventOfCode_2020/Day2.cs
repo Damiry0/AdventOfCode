18f572f8b3ccceab369d21f4dafac9f09c0671ee
Damiry0 <55887851+Damiry0@users.noreply.github.com>
2021-02-23 23:56:12 +0100

    testing of Pattern

--- AdventOfCode_2020/Day2.cs
+++ AdventOfCode_2020/Day2.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Xml.Schema;
+using System.Text.RegularExpressions;
+
 
 namespace AdventOfCode_2020
 {
     class Day2
     {
+        
+        //string sPattern = "^\\d{3}-\\d{3}-\\d{4}$";
+
+
+        public static readonly string textfilenameDay2 = @"C:\Users\damir\Desktop\Visual\AdventOfCode_2020\AdventOfCode_2020\Input1.txt";
+       /* public static List<int> ReadFile(string text)
+        {
+            int min, max;
+            char looked_character;
+            string sample;
+            string sPattern = @"^(?<min>\d{2})-(?<max>\d{2})\s\w:\s?<sample>\w+)$";
+            
+            
+            string input = @"17-19 p: pwpzpfbrcpppjppbmppp";
+            Regex r = new Regex(sPattern,RegexOptions.None);
+            Match m = r.Match(input);
+            if (m.Success)
+                Console.WriteLine(m.Result("${min}${max}"));
+           // string[] lines = File.ReadAllLines(text);
+
+
+            return result;
+        }*/
     }
+
 }
--- AdventOfCode_2020/Program.cs
+++ AdventOfCode_2020/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using static AdventOfCode_2020.Day1;
 
 
@@ -15,6 +16,18 @@ namespace AdventOfCode_2020
             (int FirstNumber, int SecondNumber) = FindingNumbersGivenSum(lines);
             Console.WriteLine("========Part One========== \nFirstNumber:{0},SecondNumber:{1},Multiplied:{2}", FirstNumber, SecondNumber, Multiply(FirstNumber, SecondNumber));
 
+
+            int min, max;
+            char looked_character;
+            string sample;
+            string sPattern = @"^(?<min>\d{2})-(?<max>\d{2})\s(?<looked>\w):\s(?<sample>\w+)$"; // XD
+
+
+            string input = @"17-19 p: pwpzpfbrcpppjppbmppp";
+            Regex r = new Regex(sPattern, RegexOptions.None, TimeSpan.FromMilliseconds(150));
+            Match m = r.Match(input);
+            if (m.Success)
+                Console.WriteLine(m.Result("${min},${max},${looked},${sample}"));
 
         }
     }
