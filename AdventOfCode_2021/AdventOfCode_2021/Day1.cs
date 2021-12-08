ee5caf79207236f42587e67c9c5d8588c0d88cda
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2021-12-08 16:20:50 +0100

    completed day one part one

---
+++ AdventOfCode_2021/AdventOfCode_2021/Day1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode_2021
+{
+    public class Day1
+    {
+        private static readonly string filename = @"./input1.txt";
+        private static List<int> ReadFileInts(string text)
+        {
+            var lines = File.ReadAllLines(text);
+            return lines.Select(int.Parse).ToList();
+        }
+
+        private static int LargerMeasurment (List<int>input)
+        {
+            var count = 0;
+            for (var i = 0; i < input.Count - 1; i++)
+            {
+                if (input[i] < input[i + 1]) count++;
+            }
+
+            return count;
+        }
+
+        public static void Day1PartOne()
+        {
+            Console.WriteLine("Output{0}",LargerMeasurment(ReadFileInts(filename)));
+        }
+    }
+}
---
+++ AdventOfCode_2021/AdventOfCode_2021/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static AdventOfCode_2021.Day1;
+
+namespace AdventOfCode_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1PartOne();
+        }
+    }
+}
