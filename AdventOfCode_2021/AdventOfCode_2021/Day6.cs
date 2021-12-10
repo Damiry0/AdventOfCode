69ce32e8ed5bef57009b942118339234babb815f
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2021-12-10 23:43:09 +0100

    Day 6 Part 2 completed

--- AdventOfCode_2021/AdventOfCode_2021/Day6.cs
+++ AdventOfCode_2021/AdventOfCode_2021/Day6.cs
@@ -7,10 +7,10 @@ namespace AdventOfCode_2021
 {
     public class Day6
     {
-        public static void Day6PartOne()
+        public static void Day6PartOne() //o(n^2) RIP
         {
             var input = File.ReadAllLines(@"./input6.txt").First().Split(",");
-            var list = input.Select(line => int.Parse(line)).ToList();
+            var list = input.Select(int.Parse).ToList();
             for (var i = 0; i < 80; i++)
             {
                 var listCount = list.Count;
@@ -21,9 +21,33 @@ namespace AdventOfCode_2021
                     list[j] = 6;
                     list.Add(8);
                 }
+                Console.WriteLine("Line:{0}",i);
             }
             Console.WriteLine("Output:{0}",list.Count);
+        }
+        public static void Day6PartTwo()
+        {
+            var input = File.ReadAllLines(@"./input6.txt").First().Split(",").Select(int.Parse).ToList();
+            
+            var fishGeneration = new long[9];
+            foreach (var i in input)
+            {
+                fishGeneration[i]++;
+            }
 
+            for (var i = 0; i < 256; i++)
+            {
+                var newOnes = fishGeneration[0];
+                for (var j = 1; j < fishGeneration.Length; j++)
+                {
+                    fishGeneration[j - 1] = fishGeneration[j];
+                }
+
+                fishGeneration[8] = newOnes;
+                fishGeneration[6] += newOnes;
+            }
+            
+            Console.WriteLine("Output:{0}",fishGeneration.Sum());
         }
         
     }
