3ade7866cb5ba7ca0004a5f307a8fe5f9f332f6c
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2021-12-11 13:06:16 +0100

    Day 7 Part 2 completed

---
+++ AdventOfCode_2021/AdventOfCode_2021/Day7.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace AdventOfCode_2021
+{
+    public class Day7
+    {
+        public static void Day7PartOne()
+        {
+            var input = File.ReadAllLines(@"./input7.txt").First().Split(",").Select(int.Parse).ToList();
+            input.Sort();
+            var median = input[500];
+            long fuel = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (input[i] < median) fuel += (median - input[i]);
+                else if (input[i] > median) fuel += (input[i] - median);
+            }
+            Console.WriteLine("Output:{0}",fuel);
+        }
+        public static void Day7PartTwo()
+        {
+            var input = File.ReadAllLines(@"./input7.txt").First().Split(",").Select(int.Parse).ToList();
+            input.Sort();
+            var median = input[500];
+            long fuel = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (input[i] < median) fuel += Sequence(median - input[i]);
+                else if (input[i] > median) fuel += Sequence(input[i] - median);
+            }
+            Console.WriteLine("Output:{0}",fuel);
+        }
+
+        private static readonly Func<long, long> Sequence = (n) => ((n*(n + 1) / 2));
+
+    }
+}
